b9cef13c10df294e19bfc6c1a819574fb5fa852d
Vinícius Adriel <82281833+ViniciusAdriel@users.noreply.github.com>
2025-11-30 13:47:08 -0300

    callbacks ready and tested

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,14 +6,55 @@ slint::slint!(
     // Go to path above to see more about UI.
 );
 
-fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()>
+{
+    // Main Window //
 
-    { // Main Window
-        // Creates Main Window from slint imports.
-        let main_window = MainWindow::new()?;
+    // Creates Main Window from slint imports.
+    let main_window = MainWindow::new()?;
 
-        main_window.run()?;
-    }
+    // pipe related callbacks
+    main_window.on_create_pipe({
+        ||{
+            // Create pipe
+            
+            // Register pipe
+            println!("Pipe Created");
+        }
+    });
+    main_window.on_remove_pipe({
+        |pipe_idx|{
+            // Remove pipe
+
+            // Deregister the pipe.
+            println!("Pipe Removed");
+        }
+    });
+
+    main_window.on_update_pipe({
+        |new_pipe|{
+            // Update pipe
+            
+            // The pipe registry is fully changed in the UI.
+            // See in /app/ui/00.MainPage/components/Card.slint
+            // In the 'Pipe Tasks' section.
+            println!("Pipe Updated");
+        }
+    });
+    main_window.on_enable_pipe({
+        |enable, pipe_idx|{
+            // Enable/Disable pipe
+
+            // The pipe registry is fully changed in the UI.
+            // See in /app/ui/00.MainPage/components/Card.slint
+            // In the 'Pipe Tasks' section.
+            println!("Pipe Enabled");
+            println!("Pipe Disabled");
+        }
+    });
+    
+    // Main loop
+    main_window.run()?;
 
     Ok(())
 }

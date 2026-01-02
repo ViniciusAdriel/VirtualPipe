b70b205d484bea384b00287b34a889897d6b6202
Vinícius Adriel <82281833+ViniciusAdriel@users.noreply.github.com>
2026-01-02 00:03:13 -0300

    ...

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd $(dirname "$0")
 mkdir -p dist
 rm -rf dist/*
 
-flatpak-builder .builddir --force-clean flatpak_manifest.yml --repo=.repo --install --user;
+flatpak-builder .builddir --force-clean flatpak_manifest.yml --repo=.repo;
 flatpak build-bundle .repo ./dist/VirtualPipe.flatpak net.viniadrii.VirtualPipe;
